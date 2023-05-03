cdde5e29960f272fdba53af99756a19d82fb94de
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-05-03 11:57:10 -0600

    Changed querySelector

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import Presenter from './presenter/presenter.js';
 import {render} from './render.js';
 
 
-const siteMainElement = document.querySelector('.main');
+const siteMainElement = document.querySelector('.page-main');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
 const FilterButtonsElement = document.querySelector('.trip-controls__filters');
-const presenter = new Presenter({container: siteMainElement});
+const presenter = new Presenter({container: tripEventsElement});
 
 render(new FilterButtonView(), FilterButtonsElement);
 
