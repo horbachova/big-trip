47478fc3207b5ae1b12f6ffbf9294737e715346c
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-05-03 11:50:18 -0600

    Added a presenter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import FilterButtonView from './view/filter-button-view.js';
+import Presenter from './presenter/presenter.js';
 import {render} from './render.js';
 
 
+const siteMainElement = document.querySelector('.main');
 const FilterButtonsElement = document.querySelector('.trip-controls__filters');
+const presenter = new Presenter({container: siteMainElement});
 
 render(new FilterButtonView(), FilterButtonsElement);
+
+presenter.init();
+
+
---
+++ src/presenter/presenter.js
@@ -0,0 +1,28 @@
+import TripListView from '../view/trip-list-view.js';
+import TripItemView from '../view/trip-item.js';
+import EditFormView from '../view/edit-form-view.js';
+import SortButtonsView from '../view/sort-button-view.js';
+import ContentContainerView from '../view/content-container-view.js';
+import { render } from '../render.js';
+
+const ITEM_COUNT = 3;
+
+export default class Presenter {
+  tripListComponent = new TripListView();
+  contentComponent = new ContentContainerView();
+
+  constructor({container}) {
+    this.container = container;
+  }
+
+  init() {
+    render(this.contentComponent, this.container);
+    render(new SortButtonsView(), this.contentComponent.getElement());
+    render(this.tripListComponent, this.contentComponent.getElement());
+    render(new EditFormView(),  this.tripListComponent.getElement());
+
+    for (let i = 0; i < ITEM_COUNT; i++) {
+      render(new TripItemView(),  this.tripListComponent.getElement());
+    }
+  }
+}
